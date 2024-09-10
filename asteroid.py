0f2b587e29f12c5fb1ffdd4b46c1fa819613f656
aaelsay3d <170895942+aaelsay3d@users.noreply.github.com>
2024-09-11 00:00:32 +0300

    Implement asteroid splitting logic on destruction

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from circleshape import CircleShape
 from constants import *
 
@@ -11,3 +12,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        else:
+            random_angle = random.uniform(20, 50)
+
+            first_vector = self.velocity.rotate(random_angle)
+            second_vector = self.velocity.rotate(-random_angle)
+
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+            first_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            second_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            first_asteroid.velocity = first_vector * 1.2
+            second_asteroid.velocity = second_vector * 1.2
