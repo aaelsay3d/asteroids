31e374cb35eca16eca477f8ec14cd67c9da4ca0b
aaelsay3d <170895942+aaelsay3d@users.noreply.github.com>
2024-09-10 18:17:39 +0300

    Make asteroids spawn and move across the screen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -9,26 +11,29 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        for object in updatable:
+            object.update(dt)
+
         screen.fill("black")
 
         for object in drawable:
             object.draw(screen)
 
-        for object in updatable:
-            object.update(dt)
-
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
